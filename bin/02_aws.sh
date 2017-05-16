40988987455bbe7e491b6897697f1823eaf5adf7
Matthew Dillon <matthewrdillon@gmail.com>
2017-05-16 11:36:07 -0700

    ENH: Refactor to streamline builds (#28)
    Addresses #27
    Fixes #13
    Fixes #21

---
+++ bin/02_aws.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+sudo tee /etc/update-motd.d/00-header <<EOF
+#!/bin/sh
+
+[ -r /etc/lsb-release ] && . /etc/lsb-release
+if [ -z "$DISTRIB_DESCRIPTION" ] && [ -x /usr/bin/lsb_release ]; then
+    DISTRIB_DESCRIPTION=$(lsb_release -s -d)
+fi
+printf "Welcome to QIIME 2 Core ${QIIME2_RELEASE}!\n\n"
+EOF
+
+sudo tee /etc/update-motd.d/10-help-text <<EOF
+#!/bin/sh
+
+printf "  * Documentation: https://docs.qiime2.org/\n"
+printf "  * Support: https://forum.qiime2.org/\n"
+EOF
+
+sudo rm /etc/update-motd.d/51-cloudguest
---
+++ bin/02_vbox.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Disable user `ubuntu` from showing up in login screen
+echo -e "[User]\nSystemAccount=true" | sudo tee /var/lib/AccountsService/users/ubuntu
+
+# Install leafpad and make default GUI text editor
+sudo apt-get install leafpad -q -y
+sudo sed -i 's/text\/plain=gedit.desktop/text\/plain=leafpad.desktop/g' /usr/share/applications/defaults.list
+
+sudo su qiime2 <<'EOF'
+  # Remove sudo banner warning
+  touch $HOME/.sudo_as_admin_successful
+
+  # Configure unity launcher
+  dbus-launch gsettings set com.canonical.Unity.Launcher favorites \
+    "['application://gnome-terminal.desktop','application://firefox.desktop','application://leafpad.desktop','application://org.gnome.Nautilus.desktop','unity://running-apps','unity://expo-icon','unity://devices']"
+
+  # Disable `amazon` apps from showing up in unity
+  mkdir -p $HOME/.local/share/applications
+  cp /usr/share/applications/ubuntu-amazon-default.desktop \
+    $HOME/.local/share/applications/ubuntu-amazon-default.desktop
+  echo "Hidden=true" >> $HOME/.local/share/applications/ubuntu-amazon-default.desktop
+EOF
